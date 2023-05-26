61ec9489f6b6731c0660fa7a772a0100424bf87e
Shrey Pandya <shrey150@yahoo.com>
2023-05-26 19:31:31 -0400

    add basic PaLM Q&A interface

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { doc, setDoc } from 'firebase/firestore';
+import { useFirestore, useFirestoreDocData } from 'reactfire';
 import { ChatBox } from './ChatBox';
 
+
 const Popup = () => {
+  const userRef = doc(useFirestore(), 'users/shrey');
+  const { status, data } = useFirestoreDocData(userRef);
+
+  const fetchAnswer = async (question: string) => {
+
+    try {
+      // Update the document with the desired data
+      await setDoc(userRef, { question })
+
+      console.log('Document written successfully!');
+    } catch (error) {
+      console.error('Error writing document:', error);
+    }
+  }
+
   return (
-    <div>
-      <ChatBox words='Type Here'></ChatBox>
-    </div>
+      <div>
+        <p>🔥 Firebase status: {JSON.stringify(status)}</p>
+        <p>🌴 PaLM status: {data?.status?.state}</p>
+        <p>
+          {
+            data?.response ?? (
+              data?.status
+                ? 'Processing...'
+                : 'Queued.'
+            )
+          }
+       </p>
+        <ChatBox onClick={fetchAnswer} />
+      </div>
   );
 };
 
--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -2,7 +2,23 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './Popup.css';
 import Popup from './Popup';
+import { FirebaseAppProvider, FirestoreProvider, useFirebaseApp } from 'reactfire';
+import { getFirestore } from 'firebase/firestore';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<Popup />);
+
+const App = () => {
+    const firestoreInstance = getFirestore(useFirebaseApp());
+    return (
+        <FirestoreProvider sdk={firestoreInstance}>
+            <Popup />
+        </FirestoreProvider>
+    )
+}
+
+root.render(
+    <FirebaseAppProvider firebaseConfig={JSON.parse(process.env.FIREBASE_CONFIG ?? '')}>
+        <App />
+    </FirebaseAppProvider>
+);
