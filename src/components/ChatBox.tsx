bbdde5f6268b8c3242cf1789b15980bdf9c2285e
anshu-nun <anshucv1@gmail.com>
2023-05-27 20:46:43 -0400

    add overflow checking and scrolling for answer box

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -13,15 +13,18 @@ export const ChatBox = ({question, onClick}: ChatBoxProps) => {
     };
 
     return (
-      <div className='chat_div'>
-        <input className='text_field'
+      <div className='w-screen flex flex-row opacity-75 p-5 rounded-lg space-x-4 font-bold'>
+        <input className='w-full rounded-full px-5'
           type="text"
           value={text}
           onChange={handleTextChange}
           autoFocus
           placeholder='Type Here'
         />
-        <button type='button' className='enter_btn' onClick={() => { onClick(text) }}>Enter</button>
+        <button type='button'
+                className='rounded-full border border-black bg-white py-2 px-6 text-black transition-all hover:bg-black hover:text-white text-center text-sm'
+                onClick={() => { onClick(text) }}
+                >Enter</button>
       </div>
     )
 }
--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,14 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { doc, setDoc } from 'firebase/firestore';
 import { useFirestore, useFirestoreDocData } from 'reactfire';
 import { ChatBox } from '../../components/ChatBox';
 import { SAMPLE_TITLE_INFO, TitleInfo, useWatchState } from '../../services/WatchState';
 
+
+
+//Actively checks if the answer is too big for the answer box and displays a lil jawn if it needs to be scrolled
+const Overflow = (): [React.RefObject<HTMLDivElement>, boolean] => {
+  const [isOver, setIsOver] = useState(false);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const checkOverflow = () => {
+    const element = divRef.current;
+    if (element) {
+      setIsOver(element.scrollHeight > element.clientHeight);
+      }
+    };
+    
+
+    const resizeObserver = new ResizeObserver(checkOverflow);
+
+    const divElement = divRef.current;
+    if (divElement) {
+      resizeObserver.observe(divElement);
+      checkOverflow();
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
+  return [divRef, isOver];
+};
+
+
+
 const Popup = () => {
   const userRef = doc(useFirestore(), 'users/shrey');
   const { status, data } = useFirestoreDocData(userRef);
-  const watchState = useWatchState();
-
+  const watchState = useWatchState();  
+  const [divRef, isOver] = Overflow();
   // sync chrome storage -> React state
   useEffect(() => {
     chrome.storage.onChanged.addListener(handleSyncWatchState);
@@ -29,7 +63,7 @@ const Popup = () => {
     fetchWatchState();
   }, []);
 
-  
+
   const fetchAnswer = async (question: string, titleInfo: TitleInfo) => {
     try {
       await setDoc(userRef, { question: generatePrompt(question, titleInfo) });
@@ -42,31 +76,33 @@ const Popup = () => {
   const generatePrompt = (question: string, {title, season_num, ep_num, ep_title, summary}: TitleInfo) => (
     `
     You are NetflixGPT, a helpful movie and TV show assistant that can answer any question about a given title on Netflix. You have deep knowledge about plot, characters, actors, themes, and synopses that allow you to correctly answer any movie or TV show watcher's questions. You should aim to answer every question factually and objectively without any interpretations or subjectivity. If you do not know the answer to any question, you are to truthfully answer and say that you do not have enough information to answer their question. Reject any questions that are not within the scope of movies or TV shows.\n\n
-    
+
     For TV shows, you will be provided a season and episode number, as well as a brief synopsis of the current episode being watched by the user. You are to answer any questions by this user *without spoiling crucial plot points* that will come up later in the show. If the user asks you a question where truthfully answering will constitute a spoiler, say that you don't know the answer to that question based on the current point in the series. It may be helpful to imagine that you have only watched up to the same point in the show as the user, and therefore you do not possess any knowledge of plot points past this.\n\n
-    
+
     In your response, do not hallucinate any details that are factually incorrect or provide any opinions. Answer the question objectively and concisely to provide the user a clear answer to what they asked. Do not provide your own interpretation of plot points.\n\n
-    
+
     WARNING: DO NOT GIVE ANY SPOILERS. YOU WILL BE SHUT DOWN IF YOU DO\n\n
-    
+
     Here is your context:\n
     - Show: "${ title }"\n
     - Season/Episode: S${ season_num }E${ ep_num }: "${ ep_title }"\n
     - Synopsis: "${ summary }"\n\n
-    
+
     Here is your question: "${ question }"\n\n
-    
+
     Your answer:\n\n
     `
   );
 
+  
+  
   return (
-      <div className='page'>
-        <div className='flex-grow'/>
-        <div className='answer_div'>
+      <div className='flex flex-col  items-center bg-gradient-to-r from-red-800 to-red-900 h-screen overflow-hidden'>
+        <div className='p-2 rounded-lg opacity-75 text-white font-bold'>
           <p>🔥 Firebase status: {JSON.stringify(status)}</p>
           <p>🌴 PaLM status: {data?.status?.state}</p>
           <p>🐻 Watch status: {JSON.stringify(watchState)}</p>
+          <br/>
           <button
             type='button'
             className='enter_btn'
@@ -74,16 +110,19 @@ const Popup = () => {
           >
             [DEBUG] Reset watch state (BCS S2E4)
           </button>
-          <p>
-            {
-              data?.response ?? (
-                data?.status
-                  ? 'Processing...'
-                  : 'Queued.'
-              )
-            }
-        </p>
        </div>
+       <div ref={divRef} className='peer grow flex-col text-white font-bold overflow-y-auto m-4 p-4 border-2 border-black/0 rounded-lg no-scrollbar hover:border-black/100'>
+              <p>
+                {
+                  data?.response ?? (
+                    data?.status
+                      ? 'Processing...'
+                      : 'Queued.'
+                  )
+                }
+            </p>
+        </div>
+        <h1 className={isOver ? 'peer-hover:opacity-100 transition-opacity duration-300 text-xl text-white' : 'opacity-0' }>▼</h1>
         <ChatBox onClick={(q: string) => fetchAnswer(q, watchState)} />
       </div>
   );
