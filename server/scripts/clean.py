265a4b89adb89805683cf4fc138f8915d6cbbca1
Tyler Yang <tsyang59@gmail.com>
2023-07-02 01:05:24 -0400

    Fixed Clean.py

--- server/scripts/clean.py
+++ server/scripts/clean.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 BASE_DIR = os.path.dirname(__file__)
+print("BASE")
 db_folder = os.path.join(BASE_DIR, '../db')
 db_pickle = os.path.join(BASE_DIR, '../db_cache.pickle')
 sources_pickle = os.path.join(BASE_DIR, '../sources.pickle')
@@ -14,7 +15,13 @@ except OSError as e:
 
 try:
     os.remove(db_pickle)
+    print(f"The file '{db_pickle}' has been deleted successfully.")
+except OSError as e:
+    print(f"Error: {e.filename} - {e.strerror}")
+
+try:
     os.remove(sources_pickle)
     print(f"The file '{sources_pickle}' has been deleted successfully.")
 except OSError as e:
     print(f"Error: {e.filename} - {e.strerror}")
+
